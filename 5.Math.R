5d1413770ea049092492ddb56f2f1e27cb47c7d2
N.V.Ratnakishor.Gade <kishor.mahi@gmail.com>
2023-04-02 20:47:23 +0530

    5.Math.R added

---
+++ 5.Math.R
@@ -0,0 +1,17 @@
+# Aim: To demonstrate basic math functions in R.
+
+# Step 1: Various Arithmetic operators in R.
+
+# Step 2: Basic math functions for sum, product, magnitude, minimum, maximum and range etc..
+
+# Step 3: Trigonometric functions.
+
+# Step 4: Logarithmic and exponential functions.
+
+# Step 5: Rounding functions. 
+
+# Step 6: Finding RMSE of two vectors.
+
+# Step 7: Special functions such as mean, factorial and sqrt etc..
+
+# Conclusion
