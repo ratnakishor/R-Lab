585d12146984b243432b79db424e5ef98e213870
N.V.Ratnakishor.Gade <kishor.mahi@gmail.com>
2023-05-07 17:04:36 +0530

    10.Graphs.R added

---
+++ 10.Graphs.R
@@ -0,0 +1,15 @@
+# Aim: To illustrate various basic graphs in R.
+
+# Step 1: Scatter plot with labels and title.
+
+# Step 2: Single and multiple line plots on a window.
+
+# Step 3: Bar plot.
+
+# Step 4: Histogram with both frequency and probability.
+
+# Step 5: Pie chart without and with labels.
+
+# Step 6: Box plots for cars dataset.
+
+# Conclusion
